2064a38618c65da0719d35b52cc60e02bde6c9ff
Allzer <dshapkarin@list.ru>
2025-12-02 16:21:27 +0300

    добавил инфу про функции

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 )
 
 func main() {
-	var user_height = 1.8
-	var user_kg float64 = 100
-	var imt = user_kg / math.Pow(user_height, 2)
-	fmt.Println((imt))
+	// var user_height float64 = 1.8
+	// var user_kg float64 = 83
+
+	imt := output_res(1.8, 83)
+	fmt.Println(imt)
+}
+
+func output_res(user_height float64, user_kg float64) float64 {
+	imt := user_kg / math.Pow(user_height/100, 2)
+	return imt
 }
