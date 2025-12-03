7bed84743f58dfc6a44c968dc35b9a259cfce45a
Allzer <dshapkarin@list.ru>
2025-12-03 14:31:22 +0300

    условный оператор

--- main.go
+++ main.go
@@ -2,18 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	// var user_height float64 = 1.8
-	// var user_kg float64 = 83
-
-	imt := output_res(1.8, 83)
-	fmt.Println(imt)
-}
-
-func output_res(user_height float64, user_kg float64) float64 {
-	imt := user_kg / math.Pow(user_height/100, 2)
-	return imt
+	age := 15
+	if age > 10 {
+		fmt.Println("курто")
+	} else {
+		fmt.Println("курто")
+	}
 }
