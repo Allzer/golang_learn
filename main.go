a8d45c1d14236a3ca73e2348104d9f0c8c0e01e0
Allzer <dshapkarin@list.ru>
2025-12-04 15:54:22 +0300

    скорректировал пример с функцией

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	output_res(1.8, 100)
 }
 
-func output_res(user_height float64, user_kg float64) {// указать тип/ы данных того, что будем возварщать. Чтобы указать несколько значений типы нужно указать в скобках {
+func output_res(user_height float64, user_kg float64) {// указать тип/ы данных того, что будем возварщать. Чтобы указать несколько значений типы нужно указать в скобках
     imt := user_kg / math.Pow(user_height/100, 2)
     fmt.Println(imt)
 }
