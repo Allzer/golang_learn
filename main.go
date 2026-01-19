f78e600f57c4e9f16864228333e091d4efaca77c
Allzer <dshapkarin@list.ru>
2026-01-19 17:32:41 +0300

    добавил инфу про горутины

--- main.go
+++ main.go
@@ -1,22 +1,21 @@
 package main
 
-type MyInterface interface {
-	Function1()
-	Function2(x int) int
-}
-
-type MyType int
-func (m MyType) Function1() {}
-func (m MyType) Function2(x int) int {
-	return x + x
-}
-
-func execute(i MyInterface) {
-	i.Function1()
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	m := MyType(1)
-	execute(m)
-	execute(&m)
+	
+	ch := make(chan int)
+
+	go func() {
+		time.Sleep(2*time.Second)
+		ch <- 42
+	}()
+
+	select {
+	case <- ch:
+		fmt.Println("Получено значение из канала")
+	}
 }
